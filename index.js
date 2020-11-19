c41a9bc749feaab199d404b06a7960a4273c8b5f
anishbhanwala <anish.bhanwala@questionpro.com>
2020-11-19 11:26:52 +0530

    First version of react, Render tag and simple components

---
+++ index.js
@@ -0,0 +1,43 @@
+const ReactDOM = {
+  render(reactElement, rootElement) {
+    rootElement.appendChild(reactElement);
+  },
+};
+
+const React = {
+  createElement(component, attributes, content) {
+    if (typeof component === "string") {
+      let element = document.createElement(component);
+      if (content) {
+        element.innerHTML = content;
+      }
+      return element;
+    }
+
+    let reactComponent = new component(attributes);
+
+    return reactComponent.render();
+  },
+  Component: class Component {
+    constructor(props) {
+      this.props = props;
+    }
+  },
+};
+
+class App extends React.Component {
+  render() {
+    return React.createElement(Header, { text: "Hello, world!" });
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    {
+      /* <h1>{this.props.text}</h1> */
+    }
+    return React.createElement("h1", {}, this.props.text);
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
